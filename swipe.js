53eb347187a72c14128bd96493b09be905b363c9
Brama Udi <bramaudi.al@gmail.com>
2020-07-09 05:54:59 +0700

    Change color if swipe is confirmed

--- swipe.js
+++ swipe.js
@@ -8,124 +8,137 @@ for (const list of lists) {
   handleStyle(list)
 
   // Get minimal swiped percent
-  const minSwipePercent = 35 // %
+  const minSwipePercent = 25 // %
   const minSwipeConfirm = minSwipePercent * list.clientWidth / 100
 
   //* Touch Event
   list.ontouchstart = ({ changedTouches }) => {
-    handleStart(changedTouches[0].screenX, list)
+    handleStart(changedTouches[0].screenX)
     list.ontouchmove = ({ changedTouches }) => {
       const { screenX } = changedTouches[0]
-      handleMotion(startX, screenX, list)
+      handleMotion(startX, screenX)
     }
   }
   list.ontouchend = ({ changedTouches }) => {
-    handleCancel(changedTouches[0].screenX, minSwipeConfirm, list)
+    handleCancel(changedTouches[0].screenX)
   }
 
   //* Mouse Event
   list.onmousedown = ({ screenX }) => {
-    handleStart(screenX, list)
+    handleStart(screenX)
     list.style.cursor = 'grabbing'
     list.onmousemove = ({ screenX }) => {
-      handleMotion(startX, screenX, list)
+      handleMotion(startX, screenX)
     }
   }
   list.onmouseup = ({ screenX }) => {
     list.onmousemove = null
     list.style.cursor = 'grab' 
-    handleCancel(screenX, minSwipeConfirm, list)
+    handleCancel(screenX)
   }
-}
 
-function handleStyle(listElm) {
-  // Parent
-  listElm.style.overflow = 'hidden'
-  listElm.style.position = 'relative'
-  listElm.style.cursor = 'grab'
-  // Each child
-  for (const child of listElm.children) {
-    child.style.float = 'left'
-    child.style.width = '100%'
-  }
-  // Left action
-  listElm.children[0].style.position = 'absolute'
-  listElm.children[0].style.right = '100%'
-  listElm.children[0].style.textAlign = 'right'
-  listElm.children[0].style.boxShadow = 'inset -3px 0 5px 0px rgba(0, 0, 0, 0.2)'
-  // Right action
-  listElm.children[2].style.position = 'absolute'
-  listElm.children[2].style.left = '100%'
-  listElm.children[2].style.boxShadow = 'inset 3px 0 5px 0px rgba(0, 0, 0, 0.2)'
-}
-
-// Set value of startX
-function handleStart(val, listElm) {
-  startX = val
-  // Reset transition on event start
-  for (const child of listElm.children) {
-    child.style.transition = 'none'
-  }
-  addition.innerText = 0 //? Debug
-  gesture.innerText = 'idle' //? Debug
-}
-
-// Move back element on cancel / not meet minSwipe % requirement
-function handleCancel(x, minSwipeConfirm, listElm) {
-  // Add transition on event end
-  for (const child of listElm.children) {
-    child.style.transition = 'margin .5s'
-  }
-  const endX = x
-  // Swipe back abortion
-  if (swipeToRight(startX, endX)) {
-    const isSwippedRight = (endX - startX) <= minSwipeConfirm
-    if (isSwippedRight) {
-      listElm.children[0].style.marginRight = 0
-      listElm.children[1].style.marginLeft = 0
-      action.innerText = 'Cancel!' //? Debug
-    } else {
-      listElm.children[0].style.marginRight = '-100%'
-      listElm.children[1].style.marginLeft = '100%'
-      action.innerText = 'Swipped right!' //? Debug
+  //* Handlers list
+  function handleStyle() {
+    // Parent
+    list.style.overflow = 'hidden'
+    list.style.position = 'relative'
+    list.style.cursor = 'grab'
+    // Each child
+    for (const child of list.children) {
+      child.style.float = 'left'
+      child.style.width = '100%'
     }
-  } else {
-    const isSwippedLeft = (startX - endX) <= minSwipeConfirm
-    if (isSwippedLeft) {
-      listElm.children[0].style.marginRight = 0
-      listElm.children[2].style.marginLeft = 0
-      listElm.children[1].style.marginLeft = 0
-      action.innerText = 'Cancel!' //? Debug
-    } else {
-      listElm.children[0].style.margin = 0
-      listElm.children[2].style.marginLeft = '-100%'
-      listElm.children[1].style.marginRight = '100%'
-      action.innerText = 'Swipped left!' //? Debug
-    }
+    // Left action
+    list.children[0].style.position = 'absolute'
+    list.children[0].style.right = '100%'
+    list.children[0].style.textAlign = 'right'
+    list.children[0].style.boxShadow = 'inset -3px 0 5px 0px rgba(0, 0, 0, 0.2)'
+    // Right action
+    list.children[2].style.position = 'absolute'
+    list.children[2].style.left = '100%'
+    list.children[2].style.boxShadow = 'inset 3px 0 5px 0px rgba(0, 0, 0, 0.2)'
   }
-  addition.innerText = 0 //? Debug
-  gesture.innerText = 'idle' //? Debug
-}
 
-// Make element move
-function handleMotion(startX, moveX, listElm) {
-  const movementX = moveX - startX
-  if (swipeToRight(startX, moveX)) {
-    // move right
-    listElm.children[1].style.marginLeft = `${movementX}px`
-    listElm.children[0].style.marginRight = `-${movementX}px` // left-act
-    // clear after left
-    listElm.children[2].style.marginLeft = 0
-    gesture.innerText = 'right' //? Debug
-  } else {
-    // move left
-    listElm.children[1].style.marginLeft = `${movementX}px`
-    listElm.children[2].style.marginLeft = `${movementX}px` // right-act
-    // clear after right
-    listElm.children[0].style.marginRight = 0
-    gesture.innerText = 'left' //? Debug
+  // Set value of startX
+  function handleStart(val) {
+    startX = val
+    // Reset transition on event start
+    for (const child of list.children) {
+      child.style.transition = 'none'
+    }
+    addition.innerText = 0 //? Debug
+    gesture.innerText = 'idle' //? Debug
   }
-  addition.innerText = (moveX - startX) //? Debug
+
+  // Move back element on cancel / not meet minSwipe % requirement
+  function handleCancel(endX) {
+    // Add transition on event end
+    for (const child of list.children) {
+      child.style.transition = 'margin .5s'
+    }
+    // Swipe back abortion
+    if (swipeToRight(startX, endX)) {
+      if (swipeConfirmed(startX, endX, 'right')) {
+        list.children[0].style.marginRight = 0
+        list.children[1].style.marginLeft = 0
+        action.innerText = 'Cancel!' //? Debug
+      } else {
+        list.children[0].style.marginRight = '-100%'
+        list.children[1].style.marginLeft = '100%'
+        action.innerText = 'Swipped right!' //? Debug
+      }
+    } else {
+      if (swipeConfirmed(startX, endX, 'left')) {
+        list.children[0].style.marginRight = 0
+        list.children[2].style.marginLeft = 0
+        list.children[1].style.marginLeft = 0
+        action.innerText = 'Cancel!' //? Debug
+      } else {
+        list.children[0].style.margin = 0
+        list.children[2].style.marginLeft = '-100%'
+        list.children[1].style.marginRight = '100%'
+        action.innerText = 'Swipped left!' //? Debug
+      }
+    }
+    addition.innerText = 0 //? Debug
+    gesture.innerText = 'idle' //? Debug
+  }
+  
+  // Make element move
+  function handleMotion(startX, moveX) {
+    const movementX = moveX - startX
+    // Moving element
+    if (swipeToRight(startX, moveX)) {
+      // move right
+      list.children[1].style.marginLeft = `${movementX}px`
+      list.children[0].style.marginRight = `-${movementX}px` // left-act
+      // clear after left
+      list.children[2].style.marginLeft = 0
+      gesture.innerText = 'right' //? Debug
+    } else {
+      // move left
+      list.children[1].style.marginLeft = `${movementX}px`
+      list.children[2].style.marginLeft = `${movementX}px` // right-act
+      // clear after right
+      list.children[0].style.marginRight = 0
+      gesture.innerText = 'left' //? Debug
+    }
+    // Change action color
+    swipeConfirmed(startX, moveX, 'right')
+      ? list.children[0].classList.remove('left-active')
+      : list.children[0].classList.add('left-active')
+    swipeConfirmed(startX, moveX, 'left')
+      ? list.children[2].classList.remove('right-active')
+      : list.children[2].classList.add('right-active')
+    addition.innerText = (moveX - startX) //? Debug
+  }
+  
+  // Detect swipe is confirmed / canceled
+  function swipeConfirmed(startX, endX, direction) {
+    return direction === 'right'
+      ? (endX - startX) <= minSwipeConfirm
+      : (startX - endX) <= minSwipeConfirm
+  }
 }
 
 // Detect swipe direction
